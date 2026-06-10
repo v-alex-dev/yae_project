28452d309c976f089891a9e4fa19105913452d71
dev_vens <vensalex1991@gmail.com>
2026-06-10 10:13:40 +0200

    Add public routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+/*
+* Public Routes : No authentification required
+*/
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+});
+
+/*
+ * Protected Routes : Authentification required
+ * Header format : Authorization: Bearer <token>
+ */
