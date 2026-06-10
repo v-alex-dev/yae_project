aeb4c115c788da658e6b37532f3fa0db20b8b98f
dev_vens <vensalex1991@gmail.com>
2026-06-10 10:00:03 +0200

    Add validation login

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -23,7 +23,8 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'email'    => ['required', 'email'],
+            'password' => ['required', 'string'],
         ];
     }
 }
